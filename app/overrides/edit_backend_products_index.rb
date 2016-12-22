953c430b111a960cad5c0a27b4c6caa2ece2a7e1
nkappesm <nkappesm@gmail.com>
2016-12-22 15:32:34 -0300

    Product decorator

--- app/overrides/edit_backend_products_index.rb
+++ app/overrides/edit_backend_products_index.rb
@@ -3,7 +3,8 @@ Deface::Override.new(:virtual_path => 'spree/admin/products/index',
   :insert_before => '[data-hook="admin_products_index_header_actions"]',
   :text => '
     <th class="text-center"><a style="pointer:cursor;"><%= Spree.t(:critical_stock) %></a></th>
-  ')
+  ',
+  :original => '1cf1e743e552f1fae35c0f3584123a8e3058e27d')
 
 Deface::Override.new(:virtual_path => 'spree/admin/products/index',
   :name => 'add_critical_stock_row_to_backend_products_index',
@@ -16,4 +17,5 @@ Deface::Override.new(:virtual_path => 'spree/admin/products/index',
       <span class="label label-ok"><i class="glyphicon glyphicon-ok"></i></span>
       <% end %>
     </td>
-  ')
+  ',
+  :original => 'b5262a1e289f4ccb3f368cb731d5a0607c5e1a94')
