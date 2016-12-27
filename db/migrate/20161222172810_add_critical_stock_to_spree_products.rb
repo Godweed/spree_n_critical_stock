462e5b681611e2c318558eee632612560c3b6fb5
nkappesm <nkappesm@gmail.com>
2016-12-27 11:37:36 -0300

    removed products has critical stock

--- db/migrate/20161222172810_add_critical_stock_to_spree_products.rb
+++
@@ -1,9 +0,0 @@
-class AddCriticalStockToSpreeProducts < ActiveRecord::Migration
-  def up
-    add_column :spree_products, :has_critical_stock, :boolean, default: false
-  end
-
-  def down
-    remove_column :spree_products, :has_critical_stock
-  end
-end
