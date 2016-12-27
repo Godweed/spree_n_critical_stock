f89fee521bbf2d1ac59fcb2c6828c0d27763f72a
nkappesm <nkappesm@gmail.com>
2016-12-27 10:56:36 -0300

    critical stock method refactor

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -1,54 +1,40 @@
 module Spree
   StockItem.class_eval do
-    after_save :check_critical_stock, if: :count_on_hand_changed?
-    after_destroy :check_critical_stock
+    after_save :new_check_critical_stock, if: :count_on_hand_changed?
+    after_destroy :new_check_critical_stock
 
     private
-
-      def check_critical_stock
-        product = self.variant.product
-        variant = self.variant
+      def new_check_critical_stock
         if variant.critical_stock.present?
-          if product.has_critical_stock == variant.has_critical_stock
-            if variant.has_critical_stock
-              if variant.critical_stock < variant.total_on_hand
-                variant.has_critical_stock = false
-                variant.save
-                check_variants_critical_stock
-              end
-            else
-              if variant.critical_stock >= variant.total_on_hand
-                Spree::CriticalStockMailer.send_warning(product, variant).deliver_now
-                variant.has_critical_stock = true
-                variant.save
-                product.has_critical_stock = true
-                product.save
+          if variant.has_critical_stock
+            if variant.critical_stock < variant.total_on_hand
+              self.variant.has_critical_stock = false
+              variant.save
+              if !variant.is_master && variant.product.variants.where(has_critical_stock: true, is_master: false).empty? && variant.product.master.has_critical_stock
+                self.variant.product.master.has_critical_stock = false
+                variant.product.master.save
               end
             end
           else
             if variant.critical_stock >= variant.total_on_hand
-              variant.has_critical_stock = true
+              Spree::CriticalStockMailer.send_warning(variant.product, variant).deliver_now
+              self.variant.has_critical_stock = true
               variant.save
+              if !variant.is_master
+                self.variant.product.master.has_critical_stock = true
+                variant.product.master.save
+              end
             end
           end
         else
           if variant.has_critical_stock
-            variant.has_critical_stock = false
+            self.variant.has_critical_stock = false
             variant.save
           end
-          if product.has_critical_stock
-            check_variants_critical_stock
+          if variant.product.master.has_critical_stock && !variant.is_master && !variant.product.variants.where(has_critical_stock: true, is_master: false)
+            self.variant.product.master.has_critical_stock = false
+            variant.product.master.save
           end
-        end
-        return
-      end
-
-      def check_variants_critical_stock
-        product = self.variant.product
-        variants = product.variants.where(has_critical_stock: true)
-        if variants.empty? && product.has_critical_stock
-          product.has_critical_stock = false
-          product.save
         end
         return
       end
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -2,67 +2,46 @@ module Spree
   Variant.class_eval do
     before_save :check_critical_stock, if: :critical_stock_changed?
     after_destroy :check_variants_critical_stock
-    
-    private
-      def check_critical_stock
-        product = self.product
-        if critical_stock.present?
-          if product.has_critical_stock == has_critical_stock
-            if has_critical_stock
-              if critical_stock < total_on_hand
-                self.has_critical_stock = false
-                check_variants_critical_stock
-              end
-            else
-              if critical_stock >= total_on_hand
-                if !new_record?
-                  Spree::CriticalStockMailer.send_warning(product, self).deliver_now
-                  self.has_critical_stock = true
-                  self.product.has_critical_stock = true
-                  product.save
-                end
-              end
-            end
-          else
-            if critical_stock >= total_on_hand
-              self.has_critical_stock = true
-            end
-          end
-        else
-          if has_critical_stock
-            self.has_critical_stock = false
-          end
-          if product.has_critical_stock
-            check_variants_critical_stock
-          end
-        end
-        return
-      end
 
-      def check_variants_critical_stock
-        product = self.product
-        variants = product.variants.where(has_critical_stock: true)
-        if product.present?
-          if variants.empty? && product.has_critical_stock
-            if product.is_master && product.master.critical_stock.present?
-              if product.master.critical_stock < product.total_on_hand
-                product.has_critical_stock = false
-                product.save
-              end
-            else
-              product.has_critical_stock = false
-              product.save
-            end
-          elsif product.is_master && !product.has_critical_stock
-            if product.master.critical_stock.present?
-              if product.master.critical_stock >= product.total_on_hand
-                product.has_critical_stock = true
-                product.save
-              end
+    def check_critical_stock
+      if critical_stock.present?
+        if critical_stock < total_on_hand && (has_critical_stock_was || !has_critical_stock_was.present?)
+          self.has_critical_stock = false
+          if product.variants.length > 0 && product.master.has_critical_stock
+            self.product.master.has_critical_stock = false
+            product.master.save
+          else
+            if !product.variants.where("has_critical_stock = true AND is_master = false AND id != ?", id).empty? && !product.master.has_critical_stock
+              self.product.master.has_critical_stock = false
+              self.product.master.save
             end
           end
+        elsif critical_stock >= total_on_hand && (!has_critical_stock_was || !has_critical_stock_was.present?)
+          if !new_record?
+            Spree::CriticalStockMailer.send_warning(product, self).deliver_now
+          end
+          self.has_critical_stock = true
+          if !product.master.has_critical_stock && product.variants.length > 0
+            self.product.master.has_critical_stock = true
+            product.master.save
+          end
         end
-        return
+      else
+        self.has_critical_stock = false if has_critical_stock
+        if product.variants.length > 0 && product.master.has_critical_stock && !product.variants.where("has_critical_stock = true AND is_master = false AND id != ?", id).empty?
+            self.product.master.has_critical_stock = false
+            product.master.save
+        end
       end
+      return
+    end
+
+    def check_variants_critical_stock
+      if product.variants.length > 0 && product.master.has_critical_stock && product.variants.where(has_critical_stock: true).empty?
+        self.product.master.has_critical_stock = false
+        product.master.save
+      end
+      return
+    end
   end
 end
